4b6c745a23bd413051e4d1916ed610dc865dcec7
Jenny Kitchen <jennykayekitchen@gmail.com>
2022-12-12 09:55:30 -0600

    project upload

---
+++ main.js
@@ -0,0 +1,8 @@
+const quarterValue = (orgNumber) => {
+    return orgNumber / 4
+}
+// Invoke the function and store the return value
+const calculatedNumber = quarterValue(20)
+
+// Log the return value
+console.log(calculatedNumber);
